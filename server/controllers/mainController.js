9f638202765778b4de4728ed9491c17875aad3f8
Supakorn <supakorn2807@protonmail.com>
2022-04-22 15:00:04 +0700

    initial commit 🎉

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -8,7 +8,12 @@ exports.homepage = async (req, res) => {
     const limitnum = 5;
     const categories = await Category.find({}).limit(limitnum);
     const latest = await Info.find({}).sort({ _id: -1 }).limit(limitnum);
-    const info = { latest };
+    const one = await Info.find({ category: "1" }).limit(limitnum);
+    const two = await Info.find({ category: "2" }).limit(limitnum);
+    const three = await Info.find({ category: "3" }).limit(limitnum);
+    const four = await Info.find({ category: "4" }).limit(limitnum);
+    const five = await Info.find({ category: "5" }).limit(limitnum);
+    const info = { latest, one, two, three, four, five };
     res.render("index", { title: "Home", categories, info });
   } catch (error) {
     res.status(500).send({ msg: error.message || "Error" });
@@ -22,6 +27,27 @@ exports.allCategories = async (req, res) => {
     // const categories = await Category.find({}).limit(limitnum);
     const categories = await Category.find({});
     res.render("categories", { title: "Home", categories });
+  } catch (error) {
+    res.status(500).send({ msg: error.message || "Error" });
+  }
+};
+
+exports.Category = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const categories = await Category.find({ category: id });
+    res.render("categories", { title: categories.name, categories });
+  } catch (error) {
+    res.status(500).send({ msg: error.message || "Error" });
+  }
+};
+
+// Info Controller
+exports.info = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const info = await Info.findById(id);
+    res.render("info", { title: info.name, info });
   } catch (error) {
     res.status(500).send({ msg: error.message || "Error" });
   }
--- server/routes/mainRoutes.js
+++ server/routes/mainRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mainController = require("../controllers/mainController");
 
 router.get("/", mainController.homepage);
+router.get("/info/:id", mainController.info);
 router.get("/categories", mainController.allCategories);
+router.get("/categories/:id", mainController.Category);
 
 module.exports = router;
